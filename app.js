d2b46f2b70140d498b2d70d2b465faf1ddf9dcdd
Vinay <1909vkk@gmail.com>
2023-09-03 10:26:05 +0530

    All features are working of this project now

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
-const apiKey="8b97fb917972bee7f29ea463d2af0b21";
-const apiUrl="https://api.openweathermap.org/data/2.5/weather?q=mumbai&units=metric";
+const apiKey = "8b97fb917972bee7f29ea463d2af0b21";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const response=await fetch(apiUrl+`&appid=${apiKey}`);
-    let result=response.json();
-    console.log(result);
-     document.querySelector(".weather-temp").innerHTML=result.main.temp;
-    document.querySelector(".weather-city").innerHTML=result.name;
-    document.querySelector(".humidity").innerHTML=result.main.humidity;
-    document.querySelector(".wind").innerHTML=result.wind.speed;
+let searchBox = document.querySelector('.search input');
+let searchBtn = document.querySelector('.search button');
+let weatherIcon = document.querySelector('.weather-icon');
+
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+    let result = await response.json();
+    
+    if(result.status==404){
+        document.querySelector(".error").style.display="block";
+    }
+
+    document.querySelector('.weather-temp').innerHTML = Math.round(result.main.temp) + "°C";
+    document.querySelector(".weather-city").innerHTML = result.name;
+    document.querySelector('.humidity').innerHTML = result.main.humidity + "%";
+    document.querySelector('.wind').innerHTML = result.wind.speed + "Km/h";
+
+    if (result.weather[0].main == "Clouds") {
+        weatherIcon.src = "images/clouds.png"
+    }
+    else if (result.weather[0].main == "Clear") {
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(result.weather[0].main=="Drizzle"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    
+    else if(result.weather[0].main=="Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
+    else if(result.weather[0].main=="Rain"){
+        weatherIcon.src = "images/rain.png";
+    }
+
 }
- 
-checkWeather()
+searchBtn.addEventListener('click', () => {
+    checkWeather(searchBox.value);
+})
+
+
+
